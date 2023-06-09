8d683e8efbf0322c6e98ca387de07aeec7b762b2
Saksham Soni <saksham.soni.mat19@itbhu.ac.in>
2023-06-09 10:32:21 +0530

    Population Added

--- worldmap/src/App.js
+++ worldmap/src/App.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useSpring, animated } from 'react-spring';
+import mapboxgl from 'mapbox-gl';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2luZzdzYWtzaGFtIiwiYSI6ImNsaTlqZ2hwdjBhZzMzZnBxbnpoNWFvOGwifQ.T509KpjVbALKdWH26tuhUQ';
 
@@ -10,56 +12,65 @@ export default function App() {
   const [lat, setLat] = useState(0);
   const [zoom, setZoom] = useState(2);
   const [geoJson, setGeoJson] = useState(null);
+  const [population, setPopulation] = useState(0);
+  const [selectedOpacity, unselectedOpacity] = [0.4, 0.001];
+  const props = useSpring({ val: population, from: { val: 0 }, config: { duration: "300" } });
 
   useEffect(() => {
     if (geoJson) {
       geoJson.features.forEach(country => {
         const id = country.properties.name;
-        if (!map.current.getSource(country.id)) {
-        map.current.addSource(id, {
-          'type': 'geojson',
-          'data': country
-        });
+        if (!map.current.getSource(id)) {
+          map.current.addSource(id, {
+            'type': 'geojson',
+            'data': country
+          });
 
-        map.current.addLayer({
-          'id': id,
-          'type': 'fill',
-          'source': id,
-          'paint': {
-            'fill-color': 'rgb(253, 92, 99)',
-            'fill-opacity': 0.001,
-            'fill-outline-color': 'rgba(253, 92, 99, 1)'
-          }
-        });
+          map.current.addLayer({
+            'id': id,
+            'type': 'fill',
+            'source': id,
+            'paint': {
+              'fill-color': 'rgb(253, 92, 99)',
+              'fill-opacity': unselectedOpacity,
+              'fill-outline-color': 'rgba(253, 92, 99, 1)'
+            }
+          });
 
-        map.current.on('click', id, (e) => {
-          const opacity = map.current.getPaintProperty(
-            id,
-            'fill-opacity'
-          );
-  
-          map.current.setPaintProperty(id, 'fill-opacity', opacity === 0.001 ? 0.4 : 0.001);
-        });
+          map.current.on('click', id, () => {
+            const opacity = map.current.getPaintProperty(
+              id,
+              'fill-opacity'
+            );
 
-        map.current.on('contextmenu', id, (e) => {
-          new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML(e.features[0].properties.name)
-            .addTo(map.current);
-        });
-  
-        map.current.on('mouseenter', id, () => {
-          map.current.getCanvas().style.cursor = 'pointer';
-        });
-  
-        map.current.on('mouseleave', id, () => {
-          map.current.getCanvas().style.cursor = '';
-        });
-  
-      }
+            if (opacity === unselectedOpacity) {
+              map.current.setPaintProperty(id, 'fill-opacity', selectedOpacity);
+              setPopulation(prevPopulation => (prevPopulation + Number(country.properties.population)));
+            } else {
+              map.current.setPaintProperty(id, 'fill-opacity', unselectedOpacity);
+              setPopulation(prevPopulation => (prevPopulation - Number(country.properties.population)));
+            }
+          });
+
+          map.current.on('contextmenu', id, (e) => {
+            new mapboxgl.Popup()
+              .setLngLat(e.lngLat)
+              .setHTML(e.features[0].properties.name)
+              .addTo(map.current);
+          });
+
+          map.current.on('mouseenter', id, () => {
+            map.current.getCanvas().style.cursor = 'pointer';
+          });
+
+          map.current.on('mouseleave', id, () => {
+            map.current.getCanvas().style.cursor = '';
+          });
+
+        }
       });
     }
-  }, [geoJson]);
+  }, [geoJson, selectedOpacity, unselectedOpacity]);
 
   useEffect(() => {
     if (!map.current) return;
@@ -79,19 +90,44 @@ export default function App() {
   });
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v12',
+      projection: 'globe',
       center: [lng, lat],
       zoom: zoom
     });
   });
 
+  function handleButtonClick(e) {
+    console.log(e);
+  }
+
   return (
     <div>
       <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div className="population">
+        Total Population
+        <animated.div>
+          {props.val.to(val => Math.floor(val))}
+        </animated.div>
+      </div>
+      <div onClick={handleButtonClick} className='map-layer'>
+        <div className='layer-switch'>
+          <button id="streets" className="side-button active">Streets</button>
+          <button className="side-button">Satellite Streets</button>
+          <button className="side-button">Outdoors</button>
+          <button className="side-button">Light</button>
+          <button className="side-button">Dark</button>
+          <button className="side-button">OSM</button>
+        </div>
+        <div className='projection-switch'>
+          <button className="side-button active">Globe</button>
+          <button className="side-button">Mercator</button>
+        </div>
       </div>
       <div ref={mapContainer} className="map-container" />
     </div>
